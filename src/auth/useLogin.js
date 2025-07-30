b463930df39d7ec8eb16809be16a28a5df9a685c
Parmod-makpower <it@makpowerindia.com>
2025-07-30 12:43:46 +0530

    login hook 4 page change

--- src/auth/useLogin.js
+++ src/auth/useLogin.js
@@ -1,10 +1,8 @@
-// auth/useLogin.js
-
 import API from "../api/axios";
 
 export const loginUser = async (mobileOrId, password) => {
   const response = await API.post("/accounts/login/", {
-    mobile_or_id: mobileOrId,  // ✅ snake_case as backend expects
+    mobile: mobileOrId, // ✅ match backend expectation
     password: password,
   });
   return response.data;
