1aabac6e1aa55a56b6ea32c130e65ce4200e5d5f
Parmod-makpower <it@makpowerindia.com>
2025-07-30 12:16:48 +0530

    1login hook change

--- src/auth/useLogin.js
+++ src/auth/useLogin.js
@@ -1,10 +1,6 @@
 import API from "../api/axios";
 
-// export const loginUser = async (mobile_or_id, password) => {
-//   const response = await API.post('/accounts/login/', { mobile_or_id, password });
-//   return response.data;
-// };
-export const loginUser = async (mobileOrId, password) => {
-  const response = await API.post('/accounts/login/', { mobile_or_id: mobileOrId, password });
+export const loginUser = async (mobile_or_id, password) => {
+  const response = await API.post('/accounts/login/', { mobile_or_id, password });
   return response.data;
 };
