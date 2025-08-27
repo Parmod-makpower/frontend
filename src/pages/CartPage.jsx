60a2af251721f7ab105d5407332be4fa3848c7ee
Parmod <it@makpowerindia.com>
2025-08-27 10:27:47 +0530

    card.jsx

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -51,7 +51,7 @@ export default function CartPage() {
   }
 
   return (
-    <div className="max-w-5xl mx-auto p-4 sm:p-6 space-y-6 mb-16">
+    <div className="max-w-5xl mx-auto py-2 px-0  sm:p-6 space-y-6 mb-16 ">
       <MobilePageHeader title="Cart" />
 
       {/* Cart Items */}
@@ -68,9 +68,11 @@ export default function CartPage() {
           );
 
           return (
+            <div className="bg-white shadow-lg p-4"
+             key={item.id}>
             <div
-              key={item.id}
-              className="bg-white rounded-xl shadow-md p-4 flex flex-col sm:flex-row gap-4 border"
+             
+              className=" flex flex-col flex-row gap-4 "
             >
               {/* Product Image */}
               <img
@@ -93,7 +95,6 @@ export default function CartPage() {
 
                 {/* Quantity */}
                 <div className="flex items-center gap-2 mt-3">
-                  <span className="text-sm font-medium">Quantity:</span>
                   <input
                     type="number"
                     min={1}
@@ -119,7 +120,25 @@ export default function CartPage() {
 
                 </div>
 
-                {/* Schemes */}
+              
+              </div>
+
+              {/* Price & Remove */}
+              <div className="flex flex-col justify-between items-end">
+               
+                <button
+                  onClick={() => handleRemove(item.id)}
+                  className="text-red-500 text-sm flex items-center gap-1 hover:text-red-700"
+                >
+                  <FaTrashAlt /> 
+                </button>
+                 <p className="font-bold text-lg">
+                  ₹{(item.price || 0) * item.quantity}
+                </p>
+              </div>
+              
+            </div>
+              {/* Schemes */}
                 {relatedSchemes.length > 0 && (
                   <div className="mt-4">
                     <div className="flex items-center gap-2 font-medium text-green-700 mb-2">
@@ -163,20 +182,6 @@ export default function CartPage() {
                     </div>
                   </div>
                 )}
-              </div>
-
-              {/* Price & Remove */}
-              <div className="flex flex-col justify-between items-end">
-                <p className="font-bold text-lg">
-                  ₹{(item.price || 0) * item.quantity}
-                </p>
-                <button
-                  onClick={() => handleRemove(item.id)}
-                  className="text-red-500 text-sm flex items-center gap-1 hover:text-red-700"
-                >
-                  <FaTrashAlt /> Remove
-                </button>
-              </div>
             </div>
           );
         })}
