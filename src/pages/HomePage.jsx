6193468f25a44ed4f47be928a98a4b9f14ede88c
Parmod <it@makpowerindia.com>
2026-01-04 17:05:05 +0530

    4januar12

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,7 +37,7 @@ export default function HomePage() {
 
   // यहाँ आप IDs की लिस्ट रखेंगे
   const trendingIds = [726, 713, 41, 82, 72, 692, 17, 46];
-  const schemeIds = [1142, 1119, 18, 28, 119, 60, 69, 1132];
+  const schemeIds = [1142, 18, 119, 60, 69,1727, 1730];
 
   return (
     <div className="mx-auto p-4 pb-25">
