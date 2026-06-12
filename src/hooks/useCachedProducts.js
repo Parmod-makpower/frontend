2fdef06f3fe8ead81519c3476fd4c057d69d7a1e
Parmod <it@makpowerindia.com>
2026-06-12 16:41:46 +0530

    12june

--- src/hooks/useCachedProducts.js
+++ src/hooks/useCachedProducts.js
@@ -23,7 +23,7 @@ export const useCachedProducts = () => {
     queryKey: ["all-products"],
     queryFn: getAllProducts,
 
-    staleTime: 1000 * 60 * 60 * 5, // 5 hour
+    staleTime: 1000 * 60 * 60 * 2, // 5 hour
     gcTime: 1000 * 60 * 60 * 24, // 24 hour cache
 
     refetchInterval: false,
