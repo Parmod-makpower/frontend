6b711734d69ed212c0864df85bfdd129e944b993
Parmod <it@makpowerindia.com>
2026-09-18 20:43:41 +0530

    latest 181

--- vite.config.js
+++ vite.config.js
@@ -56,6 +56,7 @@ export default defineConfig({
         skipWaiting: true,
         clientsClaim: true,
         cleanupOutdatedCaches: true,
+        maximumFileSizeToCacheInBytes: 3000000,
 
         runtimeCaching: [
           {
