79ef5fa7c55a84c5e210c5823172910f80f93c49
Parmod <it@makpowerindia.com>
2026-09-18 20:21:44 +0530

    18 sepeee1.0

--- src/pages/SS/ConfirmOrderPage.jsx
+++ src/pages/SS/ConfirmOrderPage.jsx
@@ -387,7 +387,7 @@ export default function ConfirmOrderPage() {
 
   // =========================================================
   // TOTAL
-  // Same calculation as original page
+  // Same calculation as original pa
   // =========================================================
   const orderTotal = selectedProducts
     .reduce(
